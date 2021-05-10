3732c886d22c396af2b6443954e28843e4be5491
fsarshad <51839755+fsarshad@users.noreply.github.com>
2021-05-10 10:07:44 -0700

    Update RankedBarChart.js

--- RankedBarChart/RankedBarChart.js
+++ RankedBarChart/RankedBarChart.js
@@ -3,7 +3,7 @@
 // Parse the Data
 d3.csv("./RankedBarChart/Titles.csv", function(data) {
 // set the dimensions and margins of the graph
-    var margin = {top: 20, right: 30, bottom: 40, left: 90},
+    var margin = {top: 40, right: 30, bottom: 60, left: 90},
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
@@ -26,16 +26,16 @@ d3.csv("./RankedBarChart/Titles.csv", function(data) {
     svg.append("text")
         .attr("class", "x label")
         .attr("text-anchor", "end")
-        .attr("x", width - 200)
-        .attr("y", height + 30)
-        .text("Titles");
+        .attr("x", width - 115)
+        .attr("y", height + 50)
+        .text("Number of Titles");
     svg.append("text")
         .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 400))
+        .attr("y", 0 - (height / 20))
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
-        .text("Number of titles won by Country");
+        .text("Countries that have won the most titles");
 
     // Y axis
     var y = d3.scaleBand()
@@ -45,16 +45,28 @@ d3.csv("./RankedBarChart/Titles.csv", function(data) {
     svg.append("g")
         .call(d3.axisLeft(y))
 
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
     //Bars
     svg.selectAll("myRect")
         .data(data)
         .enter()
         .append("rect")
+        .attr("fill", "#ffae00")
+        .style("opacity", 5)
         .attr("x", x(0) )
         .attr("y", function(d) { return y(d.Country); })
         .attr("width", function(d) { return x(d.Title); })
-        .attr("height", y.bandwidth() )
-        .attr("fill", "#69b3a2")
+        .attr("height", y.bandwidth())
+        .on("mouseover", function(d) {
+            div.transition().duration(200).style("opacity", .9);
+            div .html('Title: '+d.Title)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");})
+        .on("mouseout", function(d) {div.transition().duration(500).style("opacity", 0);})
+
 
 
     // .attr("x", function(d) { return x(d.Country); })
