a0a6ee9dd004a382b880cf4d7c70cdfa8ec95579
fsarshad <51839755+fsarshad@users.noreply.github.com>
2021-05-20 14:52:00 -0700

    Update scatter_plot.js

--- scatter_plot/scatter_plot.js
+++ scatter_plot/scatter_plot.js
@@ -3,7 +3,7 @@
 //Read the data
 d3.csv("./scatter_plot/WorldCups.csv", function(data) {
     // set the dimensions and margins of the graph
-    var margin = {top: 40, right: 30, bottom: 40, left: 50},
+    var margin = {top: 90, right: 30, bottom: 40, left: 100},
         width = 520 - margin.left - margin.right,
         height = 520 - margin.top - margin.bottom;
 
@@ -16,27 +16,53 @@ d3.csv("./scatter_plot/WorldCups.csv", function(data) {
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")")
-    // Add X axis
-    var x = d3.scaleLinear()
-        .domain([1930, 2015])
+
+
+    // X axis
+    var x = d3.scaleBand()
         .range([ 0, width ])
+        .domain(data.map(function(d) { return d.Year; }))
+        .padding(0.2);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSize(-height*1.3).ticks(20).tickFormat(d3.format(".4r")))
-        .select(".domain").remove()
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // // Add X axis
+    // var x = d3.scaleLinear()
+    //     .domain([1930, 2015])
+    //     .range([ 0, width ])
+    // svg.append("g")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x).ticks(25).tickFormat(d3.format(".4r")))
+    //     //.tickSize(-height*1).ticks(20))
+    //     .select(".domain").remove()
+
+
 
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([12, 65])
-        .range([ height, 30])
-        .nice()
+        .domain([0, 70])
+        .range([ height, 0]);
     svg.append("g")
-        .call(d3.axisLeft(y).tickSize(-width*1.3).ticks(15).tickValues(['', '15','20', '25', '30', '35', '40','45',
-            '50', '55', '60', '65']))
-        .select(".domain").remove()
+        .call(d3.axisLeft(y));
+
+    // Add Y axis
+
+    // var y = d3.scaleLinear()
+    //     .domain([12, 65])
+    //     .range([ height, 30])
+    //     .nice()
+    // svg.append("g")
+    //     .call(d3.axisLeft(y).ticks(25).tickValues(['', '15','20', '25', '30', '35', '40','45',
+    //         '50', '55', '60', '65']))
+    //     .select(".domain").remove()
+
 
     // Customization
-    svg.selectAll(".tick line").attr("stroke", "grey")
+    // svg.selectAll(".tick line").attr("stroke", "grey")
 
     // Add X axis label:
     svg.append("text")
@@ -49,25 +75,29 @@ d3.csv("./scatter_plot/WorldCups.csv", function(data) {
     svg.append("text")
         .attr("text-anchor", "end")
         .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left + 20)
-        .attr("x", -margin.top - height/2 + 100)
+        .attr("y", -margin.left + 45)
+        .attr("x", -margin.top - height/2 + 150)
         .text("MatchesPlayed")
 
     svg.append("text")
         .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 400))
+        .attr("y", -margin.left + 45)
+        // .attr("y", 0 - (margin.top / 400))
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
         .style("text-decoration", "underline")
         .text("Matches Played from 1930-2014");
+
+
     // Add dots
     svg.append('g')
         .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) { return x(d.Year); } )
-        .attr("cy", function (d) { return y(d.MatchesPlayed); } )
+        .attr("cx", function (d) { return x(d.Year);})
+        .attr("cy", function (d) { return y(d.MatchesPlayed);})
         .attr("r", 5)
         .style("fill","#619CFF")
+
 })
