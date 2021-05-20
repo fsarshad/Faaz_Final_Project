cb0c32c443eddf2d53ff9d3b1403aa07665cf9ed
fsarshad <51839755+fsarshad@users.noreply.github.com>
2021-05-20 14:26:16 -0700

    Update line_chart_code.js

--- line_chart/line_chart_code.js
+++ line_chart/line_chart_code.js
@@ -3,7 +3,7 @@ d3.csv("./line_chart/WorldCups.csv",
     // Now I can use this dataset:
     function(data) {
         // set the dimensions and margins of the graph
-        var margin = {top: 40, right: 30, bottom: 60, left: 60},
+        var margin = {top: 40, right: 80, bottom: 60, left: 60},
             width = 500 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
@@ -16,39 +16,52 @@ d3.csv("./line_chart/WorldCups.csv",
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
         // Add X axis --> it is a date format
-        var x = d3.scaleLinear()
-            .domain([1930,2014])
-            .range([ 0, width ]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).tickFormat(d3.format(".4r")));
-        svg.append("text")
-            .attr("class", "x label")
-            .attr("text-anchor", "end")
-            .attr("x", width - 200)
-            .attr("y", height + 40)
-            .text("Years");
+
+            // X axis
+            var x = d3.scaleBand()
+                .range([ 0, width ])
+                .domain(data.map(function(d) { return d.Year; }))
+                .padding(0.2);
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end");
+
+
+        // var x = d3.scaleLinear()
+        //     .domain([1930,2014])
+        //     .range([ 0, width ]);
+        // svg.append("g")
+        //     .attr("transform", "translate(0," + height + ")")
+        //     .call(d3.axisBottom(x).tickFormat(d3.format(".4r")));
+        // svg.append("text")
+        //     .attr("class", "x label")
+        //     .attr("text-anchor", "end")
+        //     .attr("x", width - 200)
+        //     .attr("y", height + 40)
+        //     .text("Years");
 
         // Add Y axis
         var y = d3.scaleLinear()
-            .domain([0, 4500000])
+            .domain([0, 4000000])
             .range([ height, 0 ]);
         svg.append("g")
             .call(d3.axisLeft(y));
 
-        // Add the line
         svg.append("path")
             .datum(data)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
+            .attr("stroke-width", 2)
             .attr("d", d3.line()
                 .x(function(d) { return x(d.Year) })
-                .y(function(d) { return y(d.Attendance) })
-            )
+                .y(function(d) { return y(d.Attendance) }))
+
         svg.append("text")
             .attr("x", (width / 2))
-            .attr("y", 0 - (height / 15))
+            .attr("y", 0 - (height / 30))
             .attr("text-anchor", "middle")
             .style("font-size", "16px")
             .style("text-decoration", "underline")
